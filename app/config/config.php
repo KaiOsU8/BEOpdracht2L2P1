ff83584557135c261b60f88d94806831c818f467
KaiOsU8 <336757@student.mboutrecht.nl>
2023-11-01 00:09:44 +0100

    first commit

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'be-mvc-opdracht2');
+define('URLROOT', 'http://be-periode1-jaar2-opdracht2.org');
